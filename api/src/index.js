0fd855634c5d1ff4c61f7ba9ac0050d790238a0a
Stefan <stefanseifeddine@gmail.com>
2025-06-06 18:06:36 +0100

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
 
+dotenv.config();
 const app = express();
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.get("/", (req, res) => {
     res.send("Blog API is up!");
