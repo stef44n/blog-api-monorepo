201a5f64c1930895e3843d4199cf184485750b4e
Stefan <stefanseifeddine@gmail.com>
2025-06-15 21:27:04 +0100

    Add loading state handling

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -2,6 +2,9 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function PrivateRoute({ children }) {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
+    console.log("user", user);
+    if (loading) return null; // or a spinner
+
     return user ? children : <Navigate to="/login" />;
 }
