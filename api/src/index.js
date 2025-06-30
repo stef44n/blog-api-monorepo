9a7973e18da1a51317074814c77c643b0b182123
Stefan <stefanseifeddine@gmail.com>
2025-06-30 22:31:11 +0100

    Debug

--- api/src/index.js
+++ api/src/index.js
@@ -24,9 +24,9 @@ app.use("/api/comments", commentRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    // app.get("*", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    // });
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
 }
 
 app.get("/", (req, res) => {
